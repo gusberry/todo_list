7918a7fa6fbb6ba36c86760203bff196264133cb
StrictUser <ermolenko198528@gmail.com>
2015-06-15 01:00:19 +0300

    Update first.js

--- first.js
+++ first.js
@@ -1,15 +1,24 @@
-var toDoList = function(name){
-    this.name = name;
-    this.func = function(){
-        document.getElementById("text").innerHTML = "<li>"+this.name+"</li>>";
-    };
-    this.addToList = function(){
-        document.getElementById('text').addEventListener("click", this.func());
-    };
-    this.delFromList = function(){
-        document.getElementsByTagName("li").removeEventListener('click', this.func());
-    };
-};
+var list = [];
+var getButton = document.getElementById("btn");
+var newList = document.getElementById("now");
 
-var things = new toDoList();
-things.addToList();
+getButton.addEventListener("click", addToList);
+
+function addToList(){
+    var liElem = document.createElement("li");
+    var inputElem = document.createElement("input");
+    inputElem.setAttribute("type", "checkbox");
+    var text = document.getElementById("text");
+    var liText = liElem.appendChild(document.createTextNode(text.value));
+    liElem.appendChild(inputElem);
+    liElem.appendChild(liText);
+    newList.appendChild(liElem);
+    list.push(liText);
+    liElem.addEventListener("click", delFromList);
+    text.value = "";
+    text.focus();
+}
+
+function delFromList(){
+    newList.removeChild(this);
+}
